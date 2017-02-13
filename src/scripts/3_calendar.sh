9da742818957064185c2caa7e0e87127f7e94fb6
kawnayeen <kawnayeen.08@gmail.com>
2017-02-13 13:48:30 +0600

    example of date added. gitignore updated

--- src/scripts/3_calendar.sh
+++ src/scripts/3_calendar.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+echo "Printing Calendar of current month"
 cal
+echo "Printing Calendar of November,2017"
+cal 11 2017
---
+++ src/scripts/4_date.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "Printing date of today"
+date
+echo "Printing today in Y-m-d format"
+date +%Y-%m-%d
