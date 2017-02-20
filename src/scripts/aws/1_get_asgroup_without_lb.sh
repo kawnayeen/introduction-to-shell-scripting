cb1000cfc95e6a4f1833bb1601f43e20d77efca8
kawnayeen <kawnayeen.08@gmail.com>
2017-02-20 22:27:59 +0600

    adding elastic load balancer to autoscaling groups which doesn't have any ELB

---
+++ src/scripts/aws/1_get_asgroup_without_lb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+INTERNAL_SERVICES_LB="XYZ"
+
+ALL_SCALING_GROUP=($(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[?HealthCheckType == `EC2`].AutoScalingGroupName' --output text))
+
+for SCALING_GROUP in "${ALL_SCALING_GROUP[@]}"
+    do
+        aws autoscaling attach-load-balancers --auto-scaling-group-name ${SCALING_GROUP} --load-balancer-names ${INTERNAL_SERVICES_LB}
+        aws autoscaling update-auto-scaling-group --auto-scaling-group-name ${SCALING_GROUP} --health-check-type ELB --health-check-grace-period 60
+done
